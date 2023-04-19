23605b4a692736351723bfd945b666cf05897e9f
spete <speterone@yandex.ru>
2023-04-19 21:03:11 +0300

    v 0.1

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-#Тестовый код
+import requests
+from bs4 import BeautifulSoup as b
+
+username = str(input())
+r = requests.get(f'https://www.dota2protracker.com/player/{username}')
+soup = b(r.text, 'html.parser')
+kk = soup.find_all(class_='played-box')
+games = soup.find_all(class_='yellow')
+games_last_8_days = str(games[0])[21:23].rstrip('<')
+well = []
+for el in kk:
+    well.append(str(el).strip('\n').split(' '))
+heroes = []
+hero = []
+winrate = []
+for i in well:
+    for el in i:
+        if 'class="green"' in str(el):
+            winrate.append(el[14:19].rstrip('%'))
+        elif 'class="yellow"' in str(el):
+            winrate.append(el[15:20].rstrip('%'))
+        elif 'class="red"' in str(el):
+            winrate.append(el[12:17].rstrip('<').rstrip('%'))
+
+for i in well:
+    for el in i:
+        if 'data=' in el:
+            heroes.append(el[6:])
+
+for el in heroes:
+    a = str(el).find('"')
+    if a != -1:
+        hero.append(el[:a])
+    elif a == -1:
+        hero.append(el)
+all = []
+print(winrate)
+print(hero)
+for i in range(len(hero)):
+    aa = f'{hero[i]}: {winrate[i]}%'
+    all.append(aa)
+print(f'Last games per 8 days: ({games_last_8_days} games)')
+for el in all:
+    print(el)
